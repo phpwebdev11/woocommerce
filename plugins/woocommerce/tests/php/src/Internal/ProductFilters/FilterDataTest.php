9a6ecfff0171aabf5b2ad0b1a4ffa2b2cab45a8b
kalessil <kalessil@gmail.com>
2026-05-19 13:24:09 +0200

    Performance: cleanup.

--- plugins/woocommerce/tests/php/src/Internal/ProductFilters/FilterDataTest.php
+++ plugins/woocommerce/tests/php/src/Internal/ProductFilters/FilterDataTest.php
@@ -90,6 +90,10 @@ class FilterDataTest extends AbstractProductFiltersTest {
 	 * @testdox Test stock counts without filter: via postmeta table.
 	 */
 	public function test_get_stock_status_counts_with_default_query_using_postmeta_table() {
+		global $wpdb;
+		// Truncate the lookup table to confirm that the underlying query is targeting the correct postmeta table.
+		$wpdb->query( "TRUNCATE TABLE {$wpdb->wc_product_meta_lookup}" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+
 		update_option( 'woocommerce_product_lookup_table_is_generating', '1' );
 		$this->test_get_stock_status_counts_with( new \WP_Query( array( 'post_type' => 'product' ) ) );
 		delete_option( 'woocommerce_product_lookup_table_is_generating' );
